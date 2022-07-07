e14ab8917fe10be436be42b8da0fa6c9f013500e
zihan987 <291067045@qq.com>
2022-07-07 14:20:06 +0800

    update demo

---
+++ src/components/iconfont/index.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  name: 'save' | 'wangge' | 'delete' | 'xiangpi' | 'flip' | 'pen' | 'quse' | 'baocun' | 'share' | 'about' | 'jiantou' | 'kafei' | 'tuoniao' | 'jiahao' | 'feedback';
+  size?: number;
+  color?: string | string[];
+  style?: React.CSSProperties;
+}
+
+declare const IconFont: FunctionComponent<Props>;
+
+export default IconFont;
